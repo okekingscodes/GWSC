1a712cbea5ced02d635c34d8bcd4d00303240f18
Odim Kingsley <okekings@gmail.com>
2023-08-02 12:51:45 +0100

    More modifications

--- js.js
+++ js.js
@@ -1,3 +1,25 @@
+// Script for navigation bar
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
+
+
+
+
 //Image slideshow script
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -27,4 +49,32 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+
+// SCRIPT FOR LOGIN AND BOOK FORM
+const loginButton = document.getElementById("login");
+const overlay = document.getElementById("overlay");
+const bookingPopup = document.getElementById("bookingPopup");
+const bookButton = document.getElementById("book");
+
+function openPopup() {
+  overlay.style.display = "block";
+  bookingPopup.style.display = "block";
+}
+
+function closePopup() {
+  overlay.style.display = "none";
+  bookingPopup.style.display = "none";
+}
+
+function bookNow() {
+  closePopup();
+  alert("You have successfully booked now!");
+}
+
+loginButton.addEventListener("click", openPopup);
+bookButton.addEventListener("click", bookNow);
 
